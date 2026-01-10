5ce4504b5dd4aa303c6eacb5af0b3ba2a3a226fe
camiP89 <campug04041@stud.noroff.no>
2026-01-10 15:55:40 +0100

    fix: clean up userInterface.js

--- js/utils/userInterface.js
+++ js/utils/userInterface.js
@@ -1,7 +1,3 @@
-export const testFunction = () => {
-  console.log("messy commit test");
-};
-
 export const isActivePath = (href, currentPath) => {
   if (href === "/") {
     return currentPath === "/" || currentPath === "/index.html";
